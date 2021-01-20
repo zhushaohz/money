42ef54f9e184841c010fd0e2383f71b6298e119a
zhushaohz <zhushaohz@163.com>
2021-01-20 12:42:05 +0800

    Initial Commit

---
+++ money/moneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  moneyApp.swift
+//  money
+//
+//  Created by zhz on 2021/1/20.
+//
+
+import SwiftUI
+
+@main
+struct moneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
